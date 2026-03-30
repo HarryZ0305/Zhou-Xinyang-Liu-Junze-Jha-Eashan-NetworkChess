3341250b421b9a5aff6ab6cac0ab56a41e1dca24
Jacenljz <jacenliu0703@gmail.com>
2026-03-30 10:36:46 -0700

    update

--- board.java
+++ board.java
@@ -1,31 +1,31 @@
 public class Board {
-    Block[][] blocks;
+    Piece[][] board;
     public Board() {
-        blocks = new Block[8][8];
+        board = new Piece[8][8];
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                blocks[i][j] = new Block(i,j);
+                board[i][j] = null;
             }
         }
         for (int i = 0; i < 8; i++) {
-            blocks[1][i].piece = new Piece(1, i, "pawn", false);
-            blocks[6][i].piece = new Piece(6, i, "pawn", true);
+            board[1][i] = new Piece(1, i, "pawn", false);
+            board[6][i] = new Piece(6, i, "pawn", true);
         }
-        blocks[0][0].piece = new Piece(0, 0, "rook", false);
-        blocks[0][7].piece = new Piece(0, 7, "rook", false);
-        blocks[7][0].piece = new Piece(7, 0, "rook", true);
-        blocks[7][7].piece = new Piece(7, 7, "rook", true);
-        blocks[0][1].piece = new Piece(0, 1, "knight", false);
-        blocks[0][6].piece = new Piece(0, 6, "knight", false);
-        blocks[7][1].piece = new Piece(7, 1, "knight", true);
-        blocks[7][6].piece = new Piece(7, 6, "knight", true);
-        blocks[0][2].piece = new Piece(0, 2, "bishop", false);
-        blocks[0][5].piece = new Piece(0, 5, "bishop", false);
-        blocks[7][2].piece = new Piece(7, 2, "bishop", true);
-        blocks[7][5].piece = new Piece(7, 5, "bishop", true);
-        blocks[0][3].piece = new Piece(0, 3, "queen", false);
-        blocks[0][4].piece = new Piece(0, 4, "king", false);
-        blocks[7][3].piece = new Piece(7, 3, "queen", true);
-        blocks[7][4].piece = new Piece(7, 4, "king", true);
+        board[0][0] = new Piece(0, 0, "rook", false);
+        board[0][7] = new Piece(0, 7, "rook", false);
+        board[7][0] = new Piece(7, 0, "rook", true);
+        board[7][7] = new Piece(7, 7, "rook", true);
+        board[0][1] = new Piece(0, 1, "knight", false);
+        board[0][6] = new Piece(0, 6, "knight", false);
+        board[7][1] = new Piece(7, 1, "knight", true);
+        board[7][6] = new Piece(7, 6, "knight", true);
+        board[0][2] = new Piece(0, 2, "bishop", false);
+        board[0][5] = new Piece(0, 5, "bishop", false);
+        board[7][2] = new Piece(7, 2, "bishop", true);
+        board[7][5] = new Piece(7, 5, "bishop", true);
+        board[0][3] = new Piece(0, 3, "queen", false);
+        board[0][4] = new Piece(0, 4, "king", false);
+        board[7][3] = new Piece(7, 3, "queen", true);
+        board[7][4] = new Piece(7, 4, "king", true);
     }
 }
--- p2.java
+++ p2.java
@@ -3,8 +3,7 @@ public class p2 {
         Board board = new Board();
         Player whitePlayer = new Player("White", true);
         Player blackPlayer = new Player("Black", false);
-        whitePlayer.pieces = new Piece[]{board.blocks[6][0].piece, board.blocks[6][1].piece, board.blocks[6][2].piece, board.blocks[6][3].piece, board.blocks[6][4].piece, board.blocks[6][5].piece, board.blocks[6][6].piece, board.blocks[6][7].piece, board.blocks[7][0].piece, board.blocks[7][1].piece, board.blocks[7][2].piece, board.blocks[7][3].piece, board.blocks[7][4].piece, board.blocks[7][5].piece, board.blocks[7][6].piece, board.blocks[7][7].piece};
-        blackPlayer.pieces = new Piece[]{board.blocks[1][0].piece, board.blocks[1][1].piece, board.blocks[1][2].piece, board.blocks[1][3].piece, board.blocks[1][4].piece, board.blocks[1][5].piece, board.blocks[1][6].piece, board.blocks[1][7].piece, board.blocks[0][0].piece, board.blocks[0][1].piece, board.blocks[0][2].piece, board.blocks[0][3].piece, board.blocks[0][4].piece, board.blocks[0][5].piece, board.blocks[0][6].piece, board.blocks[0][7].piece};
-        
+        whitePlayer.pieces = new Piece[]{board.board[6][0], board.board[6][1], board.board[6][2], board.board[6][3], board.board[6][4], board.board[6][5], board.board[6][6], board.board[6][7], board.board[7][0], board.board[7][1], board.board[7][2], board.board[7][3], board.board[7][4], board.board[7][5], board.board[7][6], board.board[7][7]};
+        blackPlayer.pieces = new Piece[]{board.board[1][0], board.board[1][1], board.board[1][2], board.board[1][3], board.board[1][4], board.board[1][5], board.board[1][6], board.board[1][7], board.board[0][0], board.board[0][1], board.board[0][2], board.board[0][3], board.board[0][4], board.board[0][5], board.board[0][6], board.board[0][7]};
     }
 }
