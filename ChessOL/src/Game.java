0a4e95cabd87eb0d606ae37fc03cd3dab48f43b8
Jacenljz <jacenliu0703@gmail.com>
2026-04-01 11:36:42 -0700

    change board structure

--- ChessOL/src/Game.java
+++ ChessOL/src/Game.java
@@ -1,24 +1,52 @@
 public class Game {
-    Board board;
+    Piece[][] board;
     Player whitePlayer, blackPlayer;
     boolean whiteTurn;
     public Game(){
-        board = new Board();
-        whiteTurn = true;
+        board = new Piece[8][8];
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(1, i, false);
+            board[6][i] = new Pawn(6, i, true);
+        }
+        board[0][0] = new Rook(0, 0, false);
+        board[0][7] = new Rook(0, 7, false);
+        board[7][0] = new Rook(7, 0, true);
+        board[7][7] = new Rook(7, 7, true);
+        board[0][1] = new Knight(0, 1, false);
+        board[0][6] = new Knight(0, 6, false);
+        board[7][1] = new Knight(7, 1, true);
+        board[7][6] = new Knight(7, 6, true);
+        board[0][2] = new Bishop(0, 2, false);
+        board[0][5] = new Bishop(0, 5, false);
+        board[7][2] = new Bishop(7, 2, true);
+        board[7][5] = new Bishop(7, 5, true);
+        board[0][3] = new Queen(0, 3, false);
+        board[7][3] = new Queen(7, 3, true);
+        board[0][4] = new King(0, 4, false);
+        board[7][4] = new King(7, 4, true);
+
         whitePlayer = new Player("White", true);
         blackPlayer = new Player("Black", false);
-        whitePlayer.pieces = new Piece[]{board.board[6][0], board.board[6][1], board.board[6][2], board.board[6][3], board.board[6][4], board.board[6][5], board.board[6][6], board.board[6][7], board.board[7][0], board.board[7][1], board.board[7][2], board.board[7][3], board.board[7][4], board.board[7][5], board.board[7][6], board.board[7][7]};
-        blackPlayer.pieces = new Piece[]{board.board[1][0], board.board[1][1], board.board[1][2], board.board[1][3], board.board[1][4], board.board[1][5], board.board[1][6], board.board[1][7], board.board[0][0], board.board[0][1], board.board[0][2], board.board[0][3], board.board[0][4], board.board[0][5], board.board[0][6], board.board[0][7]};
+        whiteTurn = true;//can change to random
     }
 
+
     public boolean canMove(int x1, int y1, int x2, int y2, boolean isWhite){
-        Piece piece = board.board[x1][y1];
-        return piece.isValidMove(x2, y2, board.board);
+        Piece piece = board[x1][y1];
+        return piece.isValidMove(x2, y2, board);
     }
 
     public void Move(int x1, int y1, int x2, int y2, boolean isWhite){
+        if(board[x2][y2] == null){
+            board[x2][y2] = board[x1][y1];
+            board[x1][y1] = null;
+        }
+    }
+
+    public void capture(int x1, int y1, int x2, int y2, boolean isWhite){
         
     }
+
     public static void main(String[] args) {
         
     }
