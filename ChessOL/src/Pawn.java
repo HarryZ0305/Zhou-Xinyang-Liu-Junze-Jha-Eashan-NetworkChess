7f56f9910bb983f42c4c319205dc52b86fe6d8ba
Harry Zhou <harryzhou935@gmail.com>
2026-04-16 11:34:44 -0700

    Fixed slight bug regarding pieces

--- ChessOL/src/Pawn.java
+++ ChessOL/src/Pawn.java
@@ -2,18 +2,17 @@ public class Pawn extends Piece {
     boolean firstMove;
 	public Pawn(int row, int col, boolean isWhite) {
         super(row, col, isWhite);
-        firstMove = true;
     }
 
     @Override
     public boolean checkRule(int toRow, int toCol, Piece[][] board) {
         
         int direction = isWhite ? -1 : 1; // white moves up, black moves down
-        if(firstMove) {
+        if(!moved) {
         	if(Math.abs(toRow - row) == 2) {
         		direction *= 2;
         	}
-        	firstMove = false;
+        	moved = true;
         }
         return toCol == col && toRow == row + direction;
     }
